73fa6c3c4890e485471113bbf06a20783ebe380d
Hikmet Kütük <hikmetkutuk@gmail.com>
2024-03-01 17:04:04 +0300

    create mobile controller

---
+++ src/main/java/com/develop/controller/MobileController.java
@@ -0,0 +1,42 @@
+package com.develop.controller;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/api/v1/mobile")
+public class MobileController {
+    List<String> mobileList = new ArrayList<>();
+
+    /**
+     * Retrieves the list of mobile devices.
+     * This method handles GET requests to "/api/v1/mobile/list" endpoint.
+     * It produces a response in plain text format.
+     *
+     * @return The list of mobile devices.
+     */
+    @GET
+    @Path("/list")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getMobileList() {
+        return Response.ok(mobileList).build();
+    }
+
+    /**
+     * Adds a new mobile device to the list.
+     * This method handles POST requests to "/api/v1/mobile/create" endpoint.
+     * It consumes plain text format.
+     *
+     * @param mobileName The name of the mobile device to be added.
+     */
+    @POST
+    @Path("/create")
+    @Consumes(MediaType.TEXT_PLAIN)
+    public Response addMobile(String mobileName) {
+        mobileList.add(mobileName);
+        return Response.ok(mobileName).build();
+    }
+}
