053830c0cf9334d3ede4f3d5b1f6a49da218a41f
Hikmet Kütük <hikmetkutuk@gmail.com>
2024-03-05 15:19:08 +0300

    add model and crud operations

---
+++ src/main/java/com/develop/model/Mobile.java
@@ -0,0 +1,14 @@
+package com.develop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Mobile {
+    int id;
+    String name;
+    String brand;
+    int ram;
+    int externalStorage;
+}
--- src/main/java/com/develop/resource/MobileResource.java
+++ src/main/java/com/develop/resource/MobileResource.java
@@ -1,15 +1,17 @@
 package com.develop.resource;
 
+import com.develop.model.Mobile;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("/api/v1/mobile")
 public class MobileResource {
-    List<String> mobileList = new ArrayList<>();
+    List<Mobile> mobileList = new ArrayList<>();
 
     /**
      * Retrieves the list of mobile devices.
@@ -20,7 +22,7 @@ public class MobileResource {
      */
     @GET
     @Path("/list")
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
     public Response getMobileList() {
         return Response.ok(mobileList).build();
     }
@@ -30,33 +32,56 @@ public class MobileResource {
      * This method handles POST requests to "/api/v1/mobile/create" endpoint.
      * It consumes plain text format.
      *
-     * @param mobileName The name of the mobile device to be added.
+     * @param mobile The mobile device to be added.
+     * @return The updated list of mobile devices.
      */
     @POST
     @Path("/create")
-    @Consumes(MediaType.TEXT_PLAIN)
-    public Response addMobile(String mobileName) {
-        mobileList.add(mobileName);
-        return Response.ok(mobileName).build();
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response addMobile(Mobile mobile) {
+        mobileList.add(mobile);
+        return Response.ok(mobile).build();
     }
+
+
+    /**
+     * Updates an existing mobile device in the list.
+     * This method handles PUT requests to "/api/v1/mobile/update/{id}" endpoint.
+     * It consumes plain text format.
+     *
+     * @param id       The ID of the mobile device to be updated.
+     * @param mobile   The updated mobile device.
+     * @return The updated list of mobile devices.
+     */
+    @PUT
+    @Path("/update/{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateMobile(@PathParam("id") int id, Mobile mobile) {
+        mobileList = mobileList.stream().map(m -> {
+            if (m.getId() == id) {
+                return mobile;
+            } else {
+                return m;
+            }
+        }).collect(Collectors.toList());
+        return Response.ok(mobileList).build();
+    }
+
 
     /**
      * Deletes a mobile device from the list.
-     * This method handles DELETE requests to "/api/v1/mobile/delete/{mobile}" endpoint.
-     * It consumes plain text format.
+     * This method handles DELETE requests to "/api/v1/mobile/delete/{id}" endpoint.
      *
-     * @param mobileName The name of the mobile device to be deleted.
+     * @param id The ID of the mobile device to be deleted.
      * @return The updated list of mobile devices.
      */
     @DELETE
-    @Path("/delete/{mobile}")
-    @Consumes(MediaType.TEXT_PLAIN)
-    public Response deleteMobile(@PathParam("mobile") String mobileName) {
-        boolean isDeleted = mobileList.remove(mobileName);
-        if (isDeleted) {
-            return Response.ok(mobileList).build();
-        } else {
-            return Response.status(Response.Status.BAD_REQUEST).build();
-        }
+    @Path("/delete/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteMobile(@PathParam("id") int id) {
+        mobileList.stream().filter(m -> m.getId() == id).findFirst().ifPresent(mobileList::remove);
+        return Response.ok(mobileList).build();
     }
 }
