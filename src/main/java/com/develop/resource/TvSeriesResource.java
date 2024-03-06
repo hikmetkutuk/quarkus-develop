5939a0e900c476d6cc790f73150a809b6677f9cb
Hikmet Kütük <hikmetkutuk@gmail.com>
2024-03-06 17:03:17 +0300

    microprofile fault tolerance

--- src/main/java/com/develop/resource/TvSeriesResource.java
+++ src/main/java/com/develop/resource/TvSeriesResource.java
@@ -6,6 +6,8 @@ import io.vertx.core.json.JsonArray;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @Path("/api/v1/tvseries")
@@ -15,8 +17,13 @@ public class TvSeriesResource {
 
     @GET
     @Path("/list/{id}")
-    public TvSeries getTvSeriesById(@PathParam("id") int id) {
-        return tvSeriesClient.getTvSeriesById(id);
+    @Fallback(fallbackMethod = "getTvSeriesByIdFallback")
+    public Response getTvSeriesById(@PathParam("id") int id) {
+        return Response.ok(tvSeriesClient.getTvSeriesById(id)).build();
+    }
+
+    public Response getTvSeriesByIdFallback(int id) {
+        return Response.ok(new TvSeries()).build();
     }
 
     @GET
