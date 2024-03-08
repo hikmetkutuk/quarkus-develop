1edc099798bc707235dc81dd4cf97474c7f412c5
Hikmet Kütük <hikmetkutuk@gmail.com>
2024-03-08 15:13:05 +0300

    add explain comments

--- src/main/java/com/develop/resource/TvSeriesResource.java
+++ src/main/java/com/develop/resource/TvSeriesResource.java
@@ -15,6 +15,12 @@ public class TvSeriesResource {
     @RestClient
     TvSeriesClient tvSeriesClient;
 
+    /**
+     * Retrieves a TV series by its ID.
+     *
+     * @param id the ID of the TV series
+     * @return the response containing the TV series
+     */
     @GET
     @Path("/list/{id}")
     @Fallback(fallbackMethod = "getTvSeriesByIdFallback")
@@ -22,10 +28,22 @@ public class TvSeriesResource {
         return Response.ok(tvSeriesClient.getTvSeriesById(id)).build();
     }
 
+    /**
+     * Retrieves a TV series by the specified ID, and provides a fallback response if the retrieval fails.
+     *
+     * @param id the ID of the TV series to retrieve
+     * @return the response containing the retrieved TV series, or a fallback response if retrieval fails
+     */
     public Response getTvSeriesByIdFallback(int id) {
         return Response.ok(new TvSeries()).build();
     }
 
+    /**
+     * Get TV series by person name.
+     *
+     * @param personName the name of the person
+     * @return a JSON array of TV series
+     */
     @GET
     @Path("/person/{personName}")
     public JsonArray getTvSeriesByPerson(@PathParam("personName") String personName) {
