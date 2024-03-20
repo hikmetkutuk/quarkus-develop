db6ce9b0efd2ef820cb431eabd09cdd546bf787e
Hikmet Kütük <hikmetkutuk@gmail.com>
2024-03-20 15:29:06 +0300

    user login

--- src/main/java/com/develop/repository/UserRepository.java
+++ src/main/java/com/develop/repository/UserRepository.java
@@ -9,4 +9,7 @@ import java.util.UUID;
 
 @ApplicationScoped
 public class UserRepository implements PanacheRepositoryBase<User, UUID> {
+    public User findByUsername(String username) {
+        return find("username", username).firstResult();
+    }
 }
--- src/main/java/com/develop/service/UserService.java
+++ src/main/java/com/develop/service/UserService.java
@@ -3,7 +3,6 @@ package com.develop.service;
 import com.develop.dto.AuthRequest;
 import com.develop.dto.AuthResponse;
 import com.develop.dto.UserRequest;
-import com.develop.model.Role;
 import com.develop.model.User;
 import com.develop.repository.UserRepository;
 import com.develop.security.PBKDF2Encoder;
@@ -12,8 +11,6 @@ import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
-
-import java.util.Collections;
 
 @Transactional
 @ApplicationScoped
@@ -30,7 +27,7 @@ public class UserService {
     public String issuer;
 
     public Response login(AuthRequest authRequest) {
-        User u = findByUsername(authRequest.username());
+        User u = userRepository.findByUsername(authRequest.username());
         if (u != null && u.getPassword().equals(passwordEncoder.encode(authRequest.password()))) {
             try {
                 return Response.ok(new AuthResponse(TokenService.generateToken(u.getUsername(), u.getRoles(), duration, issuer))).build();
@@ -42,35 +39,12 @@ public class UserService {
         }
     }
 
-    public User findByUsername(String username) {
-
-        String userUsername = "user";
-
-        //generated from password encoder
-        String userPassword = "cBrlgyL2GI2GINuLUUwgojITuIufFycpLG4490dhGtY=";
-
-        String adminUsername = "admin";
-
-        //generated from password encoder
-        String adminPassword = "dQNjUIMorJb8Ubj2+wVGYp6eAeYkdekqAcnYp+aRq5w=";
-
-        if (username.equals(userUsername)) {
-            User newUser = new User();
-            newUser.setUsername(userUsername);
-            newUser.setPassword(userPassword);
-            newUser.setRoles(Collections.singleton(Role.USER));
-            return newUser;
-        } else if (username.equals(adminUsername)) {
-            User newUser = new User();
-            newUser.setUsername(adminUsername);
-            newUser.setPassword(adminPassword);
-            newUser.setRoles(Collections.singleton(Role.ADMIN));
-            return newUser;
-        } else {
-            return null;
-        }
-    }
-
+    /**
+     * Register a new user with the provided information.
+     *
+     * @param  userRequest   the user request object containing user details
+     * @return               the response indicating the success or failure of registration
+     */
     public Response register(UserRequest userRequest) {
         User newUser = new User();
         newUser.setUsername(userRequest.username());
